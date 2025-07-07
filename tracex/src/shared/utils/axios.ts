6776b3bc872ed5e279c16cd5007aa7e3fa4dfe70
PKFO <perawat246@gmail.com>
2025-07-07 23:11:13 +0700

    TRX-101 reset project

--- tracex/src/shared/utils/axios.ts
+++
@@ -1,9 +0,0 @@
-import axios, { AxiosInstance } from "axios";
-
-const instance: AxiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-  });
-
-
-
-export default instance;
