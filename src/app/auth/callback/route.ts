8ae19a61cb3e18376b33e0402d8888eb4275aaf1
Kittipod Lambangchang <tonkittipod@gmail.com>
2025-07-29 23:19:25 +0700

    TRX-14 Fix route.ts and middleware.ts

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,4 +1,5 @@
 import { googleLogin } from '@/modules/auth/_service/auth.service';
+import { ENV } from '@/shared/config/env';
 import { PATHS } from '@/shared/config/paths';
 import { setTokenCookies } from '@/shared/utils/token';
 import { NextResponse } from 'next/server';
@@ -8,9 +9,16 @@ export async function GET(request: Request) {
 
   const code = searchParams.get('code');
   const state = searchParams.get('state');
+  const forwardedHost = request.headers.get('x-forwarded-host');
+  const forwardedProto = request.headers.get('x-forwarded-proto') || 'https';
 
   if (!code || !state) {
     console.log('Request URL', request);
+
+    if (forwardedHost) {
+      return NextResponse.redirect(`${forwardedProto}://${forwardedHost}${PATHS.login}`);
+    }
+
     return NextResponse.redirect(new URL(PATHS.login, request.url));
   }
 
@@ -21,7 +29,7 @@ export async function GET(request: Request) {
       `
       <script>
         window.location.href = '${PATHS.root}';
-      </script>
+      </script>x
     `,
       {
         headers: {
@@ -34,6 +42,10 @@ export async function GET(request: Request) {
 
     return response;
   } catch {
+    if (forwardedHost) {
+      return NextResponse.redirect(`https://${forwardedHost}${PATHS.login}`);
+    }
+
     return NextResponse.redirect(new URL(PATHS.login, request.url));
   }
 }
