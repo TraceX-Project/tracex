25a728b183c8869f96f2772d0932752ac5dd665b
Kittipod Lambangchang <tonkittipod@gmail.com>
2025-10-24 17:43:17 +0700

    TRX-108 Remove unused code

--- src/modules/attachments/_hooks/use-delete-attachment.ts
+++
@@ -1,8 +0,0 @@
-import { useMutation } from '@tanstack/react-query';
-import { deleteAttachment } from '../_services/attachments.service';
-
-export const useDeleteAttachment = () => {
-  return useMutation({
-    mutationFn: (id: string) => deleteAttachment(id),
-  });
-};
