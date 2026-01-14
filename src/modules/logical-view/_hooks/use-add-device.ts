6aae7ff01c1bdc43f761b348df0be5acac2da0b5
Kittipod Lambangchang <tonkittipod@gmail.com>
2026-01-14 22:40:40 +0700

    TRX-214 invalidate topology queries

--- src/modules/logical-view/_hooks/use-add-device.ts
+++ src/modules/logical-view/_hooks/use-add-device.ts
@@ -1,9 +1,15 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { QUERY_KEYS } from '@/shared/constants/query-key';
 import { addDeviceToProject } from '../_services/logical-view.service';
 
 export const useAddDevice = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: async ({ projectId, formData }: { projectId: string; formData: FormData }) =>
       addDeviceToProject(projectId, formData),
+    onSuccess: (_, { projectId }) => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.topology, projectId] });
+    },
   });
 };
--- src/modules/logical-view/_hooks/use-create-server.ts
+++ src/modules/logical-view/_hooks/use-create-server.ts
@@ -1,8 +1,11 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/shared/constants/query-key";
 import { CreateServerRequest } from "../_types/logical-view";
 import { createServer } from "../_services/devices.service";
 
 export const useCreateServer = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: async ({
       projectId,
@@ -11,5 +14,8 @@ export const useCreateServer = () => {
       projectId: string;
       data: CreateServerRequest;
     }) => createServer(projectId, data),
+    onSuccess: (_, { projectId }) => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.topology, projectId] });
+    },
   });
 };
