392184eafe4e08fef2b5f2b13ab0cd7e13f2f873
Kittipod Lambangchang <tonkittipod@gmail.com>
2026-01-20 19:46:58 +0700

    TRX-223 Adjust tooltip positioning and icon size, add  prop to TooltipTrigger, and update type import syntax.

--- src/modules/floors/_hooks/use-update-room.ts
+++ src/modules/floors/_hooks/use-update-room.ts
@@ -3,7 +3,7 @@ import { useMutation } from "@tanstack/react-query"
 import { updateRoom } from "../_services/rooms.service"
 import { QUERY_KEYS } from "@/shared/constants/query-key"
 import { type UpdateRoomInput } from "@/modules/rooms/_schema/schema"
-import { Room } from "@/modules/rooms/_types/room"
+import { type Room } from "@/modules/rooms/_types/room"
 
 export const useUpdateRoom = () => {
   const queryClient = getQueryClient()
