06debf481756542696fa957ed4e74c4dd6f044a6
Kittipod Lambangchang <kittipod.lambangchang@krungsrinimble.com>
2026-03-09 22:18:18 +0700

    TRX-232 Update regex to match port prefix format variations and enhance BasicInfoForm logic

--- src/shared/constants/regex.ts
+++ src/shared/constants/regex.ts
@@ -1,2 +1,2 @@
-// Matches port prefix format like "1/0", "2/1", etc.
-export const PORT_PREFIX_REGEX = /^\d+\/\d+$/;
+// Matches port prefix format like "0", "1/0", "2/1", etc.
+export const PORT_PREFIX_REGEX = /^\d+(\/\d+)*$/;
