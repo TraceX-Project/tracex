0bc40033b5aa8565b0db34d9fb9f40aa0a6bf08d
Kittipod Lambangchang <kitton.dev@gmail.com>
2026-03-16 23:58:32 +0700

    refactor: Simplify UserAvatar component by removing unused getImageProps and directly passing props to AvatarImage

--- src/shared/components/user-avatar.tsx
+++ src/shared/components/user-avatar.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
-import { getImageProps } from 'next/image';
 
 interface Props extends React.ComponentProps<typeof Avatar> {
   firstName: string;
@@ -19,18 +18,11 @@ const UserAvatar = ({
   size = 32,
   ...props
 }: Props) => {
-  const { props: nextImageProps } = getImageProps({
-    src: avatarUrl,
-    alt: alt ?? `@${firstName}`,
-    width: size,
-    height: size,
-  });
-
   const fallbackName = `${firstName.charAt(0).toUpperCase()}${lastName.charAt(0).toUpperCase()}`;
 
   return (
     <Avatar className={className} {...props}>
-      <AvatarImage {...nextImageProps} />
+      <AvatarImage src={avatarUrl} alt={alt ?? `@${firstName}`} width={size} height={size} />
       <AvatarFallback className={className}>{fallbackName}</AvatarFallback>
     </Avatar>
   );
